00426cac3fa1eac21cf8d7613b54c9c22206e8b0
sachin <nikila@codecraft.co.in>
2018-04-19 14:57:54 +0530

    ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { ViewEmployeeComponent } from './view-employee/view-employee.component';
@@ -20,6 +21,8 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     RouterModule.forRoot(
       appRoutes,
--- src/app/emp-list/emp-list.component.ts
+++ src/app/emp-list/emp-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-emp-list',
@@ -8,24 +9,89 @@ import { environment } from '../../environments/environment';
   styleUrls: ['./emp-list.component.css']
 })
 export class EmpListComponent implements OnInit {
-  empList:any=[];
+  empList: any = [];
+  //heroForm: FormGroup;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    // this.heroForm = new FormGroup({
+    //   name: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z]*')])
+    // });
     this.getEmployeeList();
   }
 
   getEmployeeList() {
     this.http.get(environment.listapi)
-    .subscribe(
-      res => {
-        this.empList=res['data'];
-      },
-      err => {
-        alert("error")
-      }
-    );
+      .subscribe(
+        res => {
+          this.empList = res['data'];
+        },
+        err => {
+          alert("error")
+        }
+      );
   }
+
+
+
+//dynamic forms
+//   dataObject = {
+//     name: {
+//             label: 'Name',
+//             value: 'Juri',
+//             type: 'text',
+//             validation: {
+//               required: true,
+//               pattern:'[a-zA-Z]*'
+//             }
+//     },
+//     age: {
+//         label: 'Age',
+//         value: 32,
+//         type: 'text',
+//         validation: {
+//           required: true,
+//           pattern:'[0-9]*'
+//         }
+//     }
+// };
+
+
+// form: FormGroup;
+// objectProps;
+
+//  ngOnInit() {
+//     this.objectProps = 
+//     Object.keys(this.dataObject)
+//         .map(prop => {
+//             return Object.assign({}, { key: prop} , this.dataObject[prop]);
+//         });
+
+//     const formGroup = {};
+//     for(let prop of Object.keys(this.dataObject)) {
+//         formGroup[prop] = new FormControl(this.dataObject[prop].value || '', this.mapValidators(this.dataObject[prop].validation));
+//     }
+
+//     this.form = new FormGroup(formGroup);
+// }
+
+// private mapValidators(validators) {
+//   const formValidators = [];
+
+//   if(validators) {
+//     for(const validation of Object.keys(validators)) {
+//       if(validation === 'required') {
+//         formValidators.push(Validators.required);
+//       } else if(validation === 'min') {
+//         formValidators.push(Validators.min(validators[validation]));
+//       } else if(validation === 'pattern') {
+//         formValidators.push(Validators.pattern(validators['pattern']));
+//       }
+//     }
+//   }
+
+//   return formValidators;
+// }
 
 }
